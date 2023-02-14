b158ab4d022192d43587b5c96de2e7465864e5ad
damodarh <dhegde50@gmail.com>
2023-02-14 21:18:05 +0000

    Deploying to gh-pages from  @ 779b6ccd94bcd6eadfb006a58f5cbb1f2d519926 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-# A repository to host my web portfolio
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50541c38ce5f6d5d23b7459320b7c223.js"
+  "/my-profile/precache-manifest.0a12030eb3f4ebcc0e56c928f4792d6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
